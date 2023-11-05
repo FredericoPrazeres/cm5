2041facb887ff8ca0e98018d8a73a9723704dc34
Frederico <fredprazeres10@gmail.com>
2023-11-05 19:12:30 +0000

    1a parte feita

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,24 +1,38 @@
 package com.example.myapplication
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.consumeAllChanges
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -26,6 +40,8 @@ import com.example.myapplication.navigation.NavGraph
 import androidx.navigation.compose.rememberNavController
 import com.example.myapplication.navigation.Screens
 import com.example.myapplication.ui.theme.MyApplicationTheme
+import kotlin.math.abs
+import kotlin.math.roundToInt
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -57,12 +73,29 @@ fun DiceWithButtonAndImage(
         else -> R.drawable.dice_6
     }
 
+
     Column(
         modifier = modifier.fillMaxSize()
             .wrapContentSize(Alignment.Center),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Image(painter = painterResource(imageResource), contentDescription = result.toString())
+        var offsetX by remember { mutableStateOf(0f) }
+        var offsetY by remember { mutableStateOf(0f) }
+
+        Image(
+            painter = painterResource(imageResource),
+            contentDescription = result.toString(),
+            modifier = Modifier
+                .offset { IntOffset(offsetX.roundToInt(), offsetY.roundToInt()) }
+                .pointerInput(Unit) {
+                    detectDragGestures { change, dragAmount ->
+                        change.consume()
+                        offsetX += dragAmount.x
+                        offsetY += dragAmount.y
+                    }
+
+                }
+        )
         Button(onClick = { result = (1..6).random() },
             modifier = Modifier.padding(8.dp)) {
             Text(stringResource(R.string.roll), fontSize = 20.sp)
@@ -83,7 +116,54 @@ fun DiceWithButtonAndImage(
         }
     }
 
+    val sensorManager = LocalContext.current.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    val accelerometerSensor: Sensor? = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+    val sensorEventListener = remember {
+        object : SensorEventListener {
 
+            var lastUpdate: Long = 0
+            var lastX = 0f
+            var lastY = 0f
+            var lastZ = 0f
+            val shakeThreshold = 800 // Adjust the threshold as needed
+
+
+            override fun onSensorChanged(event: SensorEvent?) {
+                event?.let {
+                    if (it.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+                        val currentTime = System.currentTimeMillis()
+                        val timeDifference = currentTime - lastUpdate
+
+                        if (timeDifference > 100) {
+                            val diffTime = currentTime - lastUpdate
+                            lastUpdate = currentTime
+                            val speed = abs(event.values[0] + event.values[1] + event.values[2] - lastX - lastY - lastZ) / diffTime * 10000
+
+                            if (speed > shakeThreshold) {
+                                val newValue = (1..6).random()
+                                result = newValue
+                            }
+                            lastX = event.values[0]
+                            lastY = event.values[1]
+                            lastZ = event.values[2]
+                        }
+                    }
+                }
+            }
+
+            override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+                // Do something if the accuracy of the sensor changes
+            }
+        }
+    }
+
+    LaunchedEffect(Unit) {
+        sensorManager.registerListener(
+            sensorEventListener,
+            accelerometerSensor,
+            SensorManager.SENSOR_DELAY_NORMAL
+        )
+    }
 
 }
 
